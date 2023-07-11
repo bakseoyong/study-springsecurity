c4baff6ec0e7d60ccde0610a94f788517625ae2f
bakseoyong <qwerkdud1999@gmail.com>
2023-07-11 09:54:07 +0900

    [isSuccess 필드 삭제] 이유 : 컨트롤러에서 리턴할때 상태코드를 함꼐 보내므로 해당 변수 없이도 성공여부를 확인할 수 있습니다.

--- src/main/java/com/example/login/Dto/AuthUserDto.java
+++ src/main/java/com/example/login/Dto/AuthUserDto.java
@@ -7,13 +7,11 @@ import org.springframework.http.HttpStatus;
 @Getter
 public class AuthUserDto {
     private String id;
-    private boolean isSuccess;
     private String message;
 
     @Builder
-    public AuthUserDto(String id, boolean isSuccess, String message) {
+    public AuthUserDto(String id, String message) {
         this.id = id;
-        this.isSuccess = isSuccess;
         this.message = message;
     }
 }
