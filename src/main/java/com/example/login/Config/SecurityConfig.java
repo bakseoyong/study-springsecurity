fca5aeb2d7636c9b6822f0615ff08663ff928c5c
bakseoyong <qwerkdud1999@gmail.com>
2023-07-11 16:05:47 +0900

    #1 [역할 계층구조 설정]

--- src/main/java/com/example/login/Config/SecurityConfig.java
+++ src/main/java/com/example/login/Config/SecurityConfig.java
@@ -1,13 +1,17 @@
 package com.example.login.Config;
 
+import com.example.login.Auth.SecurityHandler.*;
 import com.example.login.SecurityHandler.*;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
 
 @Configuration
 @AllArgsConstructor
@@ -26,15 +30,18 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                .expressionHandler(webSecurityExpressionHandler())
+
                 .anyRequest().authenticated();
 
-        http.formLogin()
+        http.formLogin() // 인증이 필요한 페이지가 있다면 redirect
                 .loginPage("/login/form")
                 .defaultSuccessUrl("/")
                 .failureUrl("/login/form")
                 .usernameParameter("username")
                 .passwordParameter("password")
-                .loginProcessingUrl("/api/v1/login")
+                .loginProcessingUrl("/api/v1/login") //loginProcessingUrl은 로그인 진행 시 어떤 경로에서 인증 시스템을 진행할 것인지를 설정하는 메소드이다.
+        //스프링에서는 기본 경로가 /login으로 설정되어 있으며 이 프로세스는 UserDetailsService로 향한다.
                 .successHandler(authenticationSuccess)
                 .failureHandler(authenticationFailure)
                 .permitAll();
@@ -53,6 +60,21 @@ public class SecurityConfig {
 
 
         return http.build();
+    }
+
+    @Bean
+    public RoleHierarchy roleHierarchy() {
+        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+        String hierarchy = "ROLE_ADMIN > ROLE_STAFF \n ROLE_STAFF > ROLE_USER";
+        roleHierarchy.setHierarchy(hierarchy);
+        return roleHierarchy;
+    }
+
+    @Bean
+    public DefaultWebSecurityExpressionHandler webSecurityExpressionHandler() {
+        DefaultWebSecurityExpressionHandler expressionHandler = new DefaultWebSecurityExpressionHandler();
+        expressionHandler.setRoleHierarchy(roleHierarchy());
+        return expressionHandler;
     }
 
 }
